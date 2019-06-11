c50ef42da92dc539cc8cea442fec9fe022704181
Griffin Schneider <griffinschneider@gmail.com>
2019-06-10 23:40:41 -0300

    React Native can't come to the Yarn workspace party due to symlink issues. https://github.com/facebook/metro/issues/1 Instead, exclude it from the workspace, use file dependencies for local modules, and have a script `yarn update` all local modules whenever a file changes.

--- scripts/generate-api-clients/index.ts
+++ scripts/generate-api-clients/index.ts
@@ -1,8 +1,7 @@
 import { promisify } from 'util';
 import { exec } from 'child_process';
-import * as fs from 'fs';
 import * as path from 'path';
-import * as config from './codegen-config.json';
+import console = require('console');
 
 async function proc(cwd: string, cmd: string) {
   const {stdout, stderr} = await promisify(exec)(cmd, {
@@ -16,15 +15,6 @@ async function proc(cwd: string, cmd: string) {
 (async () => {
   const configPath = path.resolve(__dirname, 'codegen-config.json');
   await proc('.', `npx small-swagger-codegen ${configPath}`);
-  const clientsDir = path.resolve(__dirname, config.output, '../');
-  const clients = fs.readdirSync(clientsDir);
-  for (const client of clients) {
-    const fullPath = path.join(clientsDir, client);
-    const stat = fs.lstatSync(fullPath);
-    if (stat.isDirectory()) {
-      console.log(fullPath)
-      await proc(fullPath, 'npm install');
-      await proc(fullPath, 'npm run build');
-    }
-  }
+  // TODO: Read package names from codegen config once it supports multiple package names.
+  await proc('.', `yarn update @common/items-api-client`);
 })();
