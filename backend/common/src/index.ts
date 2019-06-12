1e2eae1327d08e1e1419a1212b08d887cf171e79
Griffin Schneider <griffinschneider@gmail.com>
2019-06-12 16:46:07 -0300

    Move some backend logic into a common module.

---
+++ backend/common/src/index.ts
@@ -0,0 +1,16 @@
+import * as path from 'path';
+import express from 'express';
+import { Server } from 'typescript-rest';
+
+export function startup(opts: { apiName: string }) {
+  const app: express.Application = express();
+  Server.buildServices(app);
+  Server.swagger(app, {
+    endpoint: 'api-docs',
+    filePath: path.resolve(__dirname, `../../../shared/build/api-specs/${opts.apiName}-spec/swagger.json`),
+  });
+  app.listen(3000, function() {
+    console.log('Rest Server listening on port 3000!');
+  });
+}
+
--- backend/items-api/src/index.ts
+++ backend/items-api/src/index.ts
@@ -1,16 +1,5 @@
-import * as path from 'path';
-import express from 'express';
-import { Server } from 'typescript-rest';
+import { startup } from '@common/backend';
 
 // Import all controllers so their decorators get processed by typescript-rest.
 import './controllers/barrel';
-
-const app: express.Application = express();
-Server.buildServices(app);
-Server.swagger(app, {
-  endpoint: 'api-docs',
-  filePath: path.resolve(__dirname, '../../../shared/build/api-specs/items-api-spec/swagger.json'),
-});
-app.listen(3000, function() {
-  console.log('Rest Server listening on port 3000!');
-});
+startup({ apiName: 'items-api' });
