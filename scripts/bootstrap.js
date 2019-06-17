09191ecf797b45b7eb1dddb63115d213697ae446
Griffin Schneider <griffinschneider@gmail.com>
2019-06-17 01:29:52 -0300

    Workaround issues where VSCode and WebStorm couldn't find local modules by adding additional symlinks.

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -38,6 +38,11 @@ async function script() {
   // Now we have to `yarn install` again to pick up any dependencies of the api clients we just generated.
   await proc('.', 'yarn install');
 
-  await proc('./clients/mobile', 'yarn install');
+  // We're using Yarn workspaces via package.json, but that hoists all dependencies (including @common/ depdendencies)
+  // into the top-level node_modules folder. The TypeScript compiler is perfectly happy with this setup, but
+  // WebStorm/VSCode seem unable to find the @common/ modules and start throwing errors. This lerna link command
+  // creates symlinks to all @common/ dependencies in each modules' local node_modules folder. The editors seem to like
+  // this much better.
+  await proc('.', 'npx lerna link --force-local');
 }
 script();
